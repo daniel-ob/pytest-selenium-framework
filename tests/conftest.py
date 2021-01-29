5f597d55e5b59c275a3d041efdb8bf972af8a621
daniel-ob <danobrey@gmail.com>
2021-01-29 11:42:36 +0100

    HTML reports using pytest-html. Add URL and Screenshot to reports with a hook in conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,7 @@
 import pytest
 from selenium import webdriver
+from datetime import datetime
+import os
 
 
 @pytest.fixture(scope="class")
@@ -9,3 +11,31 @@ def driver_init(request):
     request.cls.driver = driver  # assign driver to class using the fixture
     yield
     driver.close()
+
+
+# Add URL and Screenshot (only on test fail) to pytest-html report.
+# Run tests from PytestSeleniumFramework folder with:
+# pytest --html=reports/report.html
+#
+# Based on from https://pytest-html.readthedocs.io/en/latest/user_guide.html
+# and https://github.com/pytest-dev/pytest-html/issues/186#issuecomment-441208344
+@pytest.hookimpl(hookwrapper=True)
+def pytest_runtest_makereport(item):
+    pytest_html = item.config.pluginmanager.getplugin("html")
+    outcome = yield
+    report = outcome.get_result()
+    extra = getattr(report, "extra", [])
+    if report.when == "call":
+        # always add url to report
+        request = item.funcargs['request']
+        driver = request.cls.driver
+        extra.append(pytest_html.extras.url(driver.current_url))
+        xfail = hasattr(report, "wasxfail")
+        if (report.skipped and xfail) or (report.failed and not xfail):
+            # only add screenshot on failure
+            timestamp = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+            working_directory = os.getcwd()
+            image_file = working_directory+'/reports/screenshot_'+timestamp+'.png'
+            driver.save_screenshot(image_file)
+            extra.append(pytest_html.extras.image(image_file))
+        report.extra = extra
--- tests/test_home.py
+++ tests/test_home.py
@@ -12,11 +12,11 @@ class TestHome:
 
     def test_search(self):
         wait = WebDriverWait(self.driver, 6)
-        search_text = "shirt"
+        search_text = "Dress"
         self.driver.find_element(By.ID, "search_query_top").send_keys(search_text)
         self.driver.find_element(By.CLASS_NAME, "button-search").click()
         wait.until(EC.presence_of_element_located((By.CLASS_NAME, "product-listing")))
         search_results = self.driver.find_elements(By.CSS_SELECTOR, "div[class='right-block'] .product-name")
         for product in search_results:
-            # print(product.text)
+            print(product.text)
             assert search_text in product.text
