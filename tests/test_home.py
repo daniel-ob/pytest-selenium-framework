4d1d93391e60f4c8b8283b8962641adfd5a0c995
daniel-ob <danobrey@gmail.com>
2021-02-01 13:42:34 +0100

    Parameterize test_search

--- tests/test_home.py
+++ tests/test_home.py
@@ -9,10 +9,12 @@ class TestHome:
         homepage.load()
         assert self.driver.title == "My Store"
 
-    def test_search(self):
+    @pytest.mark.parametrize("text", ["shirt", "Dress"])
+    def test_search(self, text):
         # Make a search and check that founded items matches that search
         homepage = HomePage(self.driver)
-        search_text = "Dress"
+        homepage.load()
+        search_text = text
         search_results = homepage.search(search_text)
         for product in search_results:
             print(product.text)
